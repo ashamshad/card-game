d22d8f8ba8fdc4953debd26bcdb886e31d852af1
Adil Shamshad <adilshamshad@MacBook-Pro-de-Adil.local>
2015-02-12 20:12:06 +0000

    Delete files that should not be in remote repo.

--- target/doublesir-1.0-SNAPSHOT/app/app.js
+++
@@ -1,9 +0,0 @@
-(function(angular) {
-  angular.module("gameApp", [
-    "gameApp.controllers",
-    "gameApp.services"
-  ]);
-
-  angular.module("gameApp.controllers", []);
-  angular.module("gameApp.services", []);
-})(angular);
